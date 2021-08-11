e2eee9340ada7050eb58b8944949d569a6e74f72
kkaris <karis.klas@gmail.com>
2021-08-10 21:06:41 -0400

    Add nsid keys as lowercase

--- indra_network_search/autocomplete/autocomplete.py
+++ indra_network_search/autocomplete/autocomplete.py
@@ -55,7 +55,7 @@ class NodesTrie(SortedStringTrie):
         _is_str_nodes(graph)
         return cls(
             **{
-                f'{graph.nodes[n]["ns"]}:{graph.nodes[n]["id"]}': (
+                f'{graph.nodes[n]["ns"]}:{graph.nodes[n]["id"]}'.lower(): (
                     n,
                     graph.nodes[n]["ns"],
                     graph.nodes[n]["id"],
