f385c7c85215285f19c5570ec4f659370913c429
kkaris <karis.klas@gmail.com>
2021-08-19 17:56:10 -0400

    Add hash blacklist check in Dijkstra

--- indra_network_search/result_handler.py
+++ indra_network_search/result_handler.py
@@ -498,8 +498,7 @@ class DijkstraResultManager(PathResultManager):
         if self.filter_options.curated_db_only and not stmt_dict['curated']:
             return False
 
-        if self.filter_options.hash_blacklist and \
-                stmt_dict['stmt_hash'] in self.filter_options.hash_blacklist:
+        if self._hash_blacklist and stmt_dict['stmt_hash'] in self._hash_blacklist:
             return False
 
         return True
