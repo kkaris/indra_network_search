bf475f4540a43b31f3119b5618464b5d8155cbaa
kkaris <karis.klas@gmail.com>
2021-08-24 21:10:06 -0400

    Add MultiInteractorsOptions

--- indra_network_search/data_models/__init__.py
+++ indra_network_search/data_models/__init__.py
@@ -329,6 +329,21 @@ class OntologyOptions(BaseModel):
     is_a_part_of: Optional[Set[str]] = None
 
 
+class MultiInteractorsOptions(BaseModel):
+    """Arguments for indra_network_search.pathfinding.direct_multi_interactors"""
+
+    interactor_list: List[StrNode]
+    downstream: bool
+    allowed_ns: Optional[List[str]] = None
+    stmt_types: Optional[List[str]] = None
+    source_filter: Optional[List[str]] = None
+    max_results: int = 50
+    hash_blacklist: Optional[Set[int]] = None
+    node_blacklist: Optional[List[str]] = None
+    belief_cutoff: float = 0.0
+    curated_db_only: bool = False
+
+
 # Models and sub-models for the Results
 class Node(BaseModel):
     """Data for a node"""
