61b6c7d8191ede881cebd13e7df6d36c1eb8cf84
kkaris <karis.klas@gmail.com>
2021-09-07 15:50:32 -0400

    WIP: first iteration of server status box

---
+++ indra_network_search/frontend/src/components/status_box/StatusBox.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="container">
+    show status and info
+  </div>
+</template>
+
+<script>
+export default {
+  name: "StatusBox.vue",
+  data() {
+    return {
+      // Follows indra_network_search.data_models.rest_models.ServerStatus
+      unsigned_nodes: 0,
+      signed_nodes: 0,
+      unsigned_edges: 0,
+      signed_edges: 0,
+      status: '',
+      graph_date: '',
+    }
+  },
+  methods: {
+    checkStatus() {
+      // Call /status
+      return {}
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
