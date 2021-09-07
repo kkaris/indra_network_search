3e20e8d642b2d745c50f4934c4e55699831ec122
kkaris <karis.klas@gmail.com>
2021-09-07 13:29:45 -0400

    Update Health model, add ServerStatus model

--- indra_network_search/data_models/rest_models.py
+++ indra_network_search/data_models/rest_models.py
@@ -3,9 +3,26 @@ Contains return models from the rest api
 """
 from pydantic import BaseModel
 
-__all__ = ['Health']
+try:
+    from typing import Literal, List
+except ImportError:
+    from typing_extensions import Literal
+
+__all__ = ["Health"]
 
 
 class Health(BaseModel):
     """Health status"""
-    status: str
+
+    status: Literal["booting", "available"]
+
+
+class ServerStatus(BaseModel):
+    """Status with more detail than health"""
+
+    graphs_available: List[Literal["signed", "unsigned"]]
+    unsigned_nodes: int
+    signed_nodes: int
+    unsigned_edges: int
+    signed_edges: int
+    unique_entitity_count: int
