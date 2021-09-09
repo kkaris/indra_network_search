edeb935b92b0314fe965f4f2790f343c430780fd
kkaris <karis.klas@gmail.com>
2021-09-09 17:39:01 -0400

    Format about page

--- indra_network_search/frontend/src/views/About.vue
+++ indra_network_search/frontend/src/views/About.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="about">
     <h1>The INDRA Network Search</h1>
-    <p>The INDRA Network Search is a project that centers around making a knowledge graph representation of the INDRA DataBase searchable</p>
+    <p>
+      The INDRA Network Search is a project that centers around making a
+      knowledge graph representation of the INDRA DataBase searchable
+    </p>
   </div>
 </template>
