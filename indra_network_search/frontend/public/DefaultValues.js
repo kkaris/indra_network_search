1455dc95b414c8f07236329b172c5abd399f9163
kkaris <karis.klas@gmail.com>
2021-09-23 12:03:30 -0400

    Move empty results to DefaultValues.js

--- indra_network_search/frontend/public/DefaultValues.js
+++ indra_network_search/frontend/public/DefaultValues.js
@@ -5,6 +5,17 @@ const CONST_C = 1;
 const CONST_TK = 10;
 const USER_TIMEOUT = 30;
 const WEIGHTED = "unweighted";
+const EMPTY_RESULTS = {
+    query_hash: "",
+    time_limit: 30.0,
+    timed_out: false,
+    hashes: [],
+    path_results: {},
+    reverse_path_results: {},
+    ontology_results: {},
+    shared_target_results: {},
+    shared_regulators_results: {},
+}
 
 export default {
     DEPTH_LIMIT,
@@ -14,4 +25,5 @@ export default {
     CONST_TK,
     USER_TIMEOUT,
     WEIGHTED,
+    EMPTY_RESULTS,
 }
