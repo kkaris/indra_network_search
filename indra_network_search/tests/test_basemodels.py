6780ee6f7df54cf5c3414277d3ad15490d5d4229
kkaris <karis.klas@gmail.com>
2021-10-04 21:47:39 -0400

    Add test for StmtData w ontological edge

---
+++ indra_network_search/tests/test_basemodels.py
@@ -0,0 +1,18 @@
+from indra_network_search.data_models import StmtData
+
+
+def test_stmt_data():
+    stmt_dict = {
+        "stmt_type": "fplx",
+        "evidence_count": 1,
+        "source_counts": {"fplx": 1},
+        "stmt_hash": "https://identifiers.org/fplx:FANC",
+        "belief": 1.0,
+        "weight": 1e-15,
+        "curated": True,
+        "english": "FPLX:FANC is an ontological parent of HGNC:1100",
+        "z_score": 0,
+        "corr_weight": 1,
+    }
+    stmt_data = StmtData(db_url_hash=stmt_dict["stmt_hash"], **stmt_dict)
+    assert stmt_data.source_counts == {"fplx": 1}
