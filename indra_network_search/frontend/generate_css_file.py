baa3223821c4e652fcf5ffc7e5cf0636237a50ef
kkaris <karis.klas@gmail.com>
2021-11-08 13:40:50 -0500

    Use __file__ instead of setting relative path with '.'

--- indra_network_search/frontend/generate_css_file.py
+++ indra_network_search/frontend/generate_css_file.py
@@ -5,7 +5,7 @@ from indra.assemblers.html.assembler import generate_source_css
 
 logger = logging.getLogger(__name__)
 
-CSS_PATH = Path("./src/assets/sources.css").absolute().as_posix()
+CSS_PATH = Path(__file__).absolute().parent.joinpath("src/assets/sources.css").as_posix()
 
 if __name__ == "__main__":
     logger.info(f"Regenerating source CSS at {CSS_PATH}")
