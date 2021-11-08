0efeba1b41b747d1e67972cf7aa2bde40f5f9460
kkaris <karis.klas@gmail.com>
2021-11-08 13:31:54 -0500

    Add Field descriptors for ServerStatus fields

--- indra_network_search/data_models/rest_models.py
+++ indra_network_search/data_models/rest_models.py
@@ -4,7 +4,7 @@ Contains return models from the rest api
 from datetime import date
 from typing import Optional
 
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 try:
     from typing import Literal
@@ -26,9 +26,9 @@ class Health(BaseModel):
 class ServerStatus(BaseModel):
     """Status with more detail than health"""
 
-    unsigned_nodes: Optional[int] = None
-    signed_nodes: Optional[int] = None
-    unsigned_edges: Optional[int] = None
-    signed_edges: Optional[int] = None
-    graph_date: Optional[date] = None
-    status: STR_STATUS
+    unsigned_nodes: Optional[int] = Field(None, description="Number of " "unsigned nodes " "in the graph")
+    signed_nodes: Optional[int] = Field(None, description="Number of signed " "nodes in the graph")
+    unsigned_edges: Optional[int] = Field(None, description="Number of " "unsigned edges " "in the graph")
+    signed_edges: Optional[int] = Field(None, description="Number of signed " "edges in the graph")
+    graph_date: Optional[date] = Field(None, description="Date of the " "graph creation from the database")
+    status: STR_STATUS = Field(..., description="Status of the server")
