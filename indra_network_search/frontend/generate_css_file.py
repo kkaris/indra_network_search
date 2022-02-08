0a803e887282ef4fc1ab8759ca5db6e7e535e25f
kkaris <karis.klas@gmail.com>
2022-02-08 12:56:43 -0500

    Also write resource file with CSS update script

--- indra_network_search/frontend/generate_css_file.py
+++ indra_network_search/frontend/generate_css_file.py
@@ -1,12 +1,29 @@
 import logging
 from pathlib import Path
 
-from indra.assemblers.html.assembler import generate_source_css
-
 logger = logging.getLogger(__name__)
 
-CSS_PATH = Path(__file__).absolute().parent.joinpath("src/assets/sources.css").as_posix()
+ASSETS = Path(__file__).absolute().parent.joinpath("src/assets")
+CSS_PATH = ASSETS.joinpath("sources.css").as_posix()
+SOURCE_PATH = ASSETS.joinpath("source_list.json")
 
-if __name__ == "__main__":
+
+def update_sources_list_json():
+    """Update the sources list JSON file read by SourceDisplay.vue."""
+    from indra_db_service.api import sources_dict
+    import json
+    with SOURCE_PATH.open('w') as fh:
+        json.dump(sources_dict, fh, indent=2)
+
+
+def main():
+    from indra.assemblers.html.assembler import generate_source_css
+
+    logger.info("Updating source lists")
+    update_sources_list_json()
     logger.info(f"Regenerating source CSS at {CSS_PATH}")
     generate_source_css(fname=CSS_PATH)
+
+
+if __name__ == "__main__":
+    main()
