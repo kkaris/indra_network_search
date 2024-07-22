f5bdf2cfca29801b07e8ee076292d22f63d177ca
kkaris <karis.klas@gmail.com>
2024-07-22 09:18:00 -0700

    Update graph date

--- indra_network_search/rest_api.py
+++ indra_network_search/rest_api.py
@@ -59,7 +59,7 @@ DEBUG = (environ.get("API_DEBUG") or "").lower() in TRUTHINESS
 USE_GRAPH_CACHE = environ.get("USE_CACHE") in TRUTHINESS
 CACHE_RESULTS = environ.get("CACHE_RESULTS") in TRUTHINESS
 HEALTH = Health(status="booting")
-STATUS = ServerStatus(status="booting", graph_date="2022-01-11")
+STATUS = ServerStatus(status="booting", graph_date="2023-07-26")
 network_search_api: IndraNetworkSearchAPI
 nsid_trie: NodesTrie
 nodes_trie: NodesTrie
